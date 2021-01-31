9f15f3082ee5525e1b2b17dea5bdae2bece6102d
Niels <niels@datatrics.com>
2021-01-31 14:54:10 +0100

    Add Navbar component

---
+++ src/components/navbar/children/NavbarMobileButton.component.vue
@@ -0,0 +1,48 @@
+<template>
+ <div 
+  class="relative w-12 h-12 cursor-pointer z-20 hamburger-button"
+  :class="{ 'active': active }"
+  @click="onClickHandler">
+    <span 
+      v-for="index in 3"
+      :key="index"
+      :class="[
+        'bg-white',
+        'absolute',
+        'transform-gpu',
+        '-translate-x-2/4',
+        '-translate-y-2/4',
+        'left-1/2',
+        'w-1/2',
+        'h-0.5',
+        'transition-all',
+        'duration-200',
+        'ease-in-out',
+        'rounded',
+      ]" />
+  </div>  
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NavbarMobileButton',
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  setup (props, { emit }) {
+
+    const onClickHandler = () => emit('toggle', !props.active)
+    
+    return { onClickHandler }
+  },
+})
+</script>
+
+<style lang="scss">
+@import './NavbarMobileButton.style';
+</style>
---
+++ src/components/navbar/children/NavbarMobileMenu.component.vue
@@ -0,0 +1,27 @@
+<template>
+  <teleport to=".modal-container">
+    <div class="fixed z-10 inset-0 overflow-y-auto">
+      <div class="flex items-center justify-center min-h-screen px-4 py-20 text-center">
+        <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div class="absolute inset-0 bg-gray-600"></div>
+        </div>
+        <div class="text-left overflow-hidden transform transition-all">
+          <router-link
+            :to="{ name: 'Home' }"
+            v-slot="{ href, navigate, isActive }"
+          >
+            <div :active="isActive" :href="href" @click="navigate"
+              >Home</div>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+
+})
+</script>
